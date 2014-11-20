10e4d6af467b16745be4c5c888f5bc92980972e1
Troy C. Thompson <troy.thompson@cognitivenetworks.com>
2014-11-20 11:41:40 -0800

    added function to return the instance name

--- awscom.py
+++ awscom.py
@@ -48,18 +48,42 @@ def getid(private_ip):
   #Command execution
   procout = subprocess.Popen([LOCAL_AWS, LOCAL_AWS_SERVICE, LOCAL_AWS_ARG1, LOCAL_AWS_ARG2, LOCAL_AWS_ARG3],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
   myset=list(procout.stdout)
+  #Iterate through the line items for the string that denotes this is the line containg the instance-id information
   for item in myset:
     searchstr = 'INSTANCES'
     if searchstr in item:
       targetlist = item.split()
       myinstanceid = targetlist[7]
-      print myinstanceid
   #mytargetline = re.sub(r'\s+', ' ', myset[2])
   #mytargetlist = mytargetline.split()
   #myinstanceid = mytargetlist[7]
-  #return myinstanceid
+  return myinstanceid
   
+# Function to obtain the instance name
+def getname(private_ip):
 
+  #Obtain instance id
+  myinstanceid = getid(private_ip)
+
+  #Command setup
+  LOCAL_AWS = 'aws'
+  LOCAL_AWS_SERVICE = 'ec2'
+  LOCAL_AWS_ARG1 = 'describe-instances'
+  DRYRUN = '--dry-run'
+  LOCAL_AWS_ARG2 = '--filters'
+  LOCAL_AWS_ARG3 = 'Name=private-ip-address,Values=%s' % private_ip
+
+  #Command execution
+  procout = subprocess.Popen([LOCAL_AWS, LOCAL_AWS_SERVICE, LOCAL_AWS_ARG1, LOCAL_AWS_ARG2, LOCAL_AWS_ARG3],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+  myset = list(procout.stdout)
+  
+  #Iterate through the line items for the string that denotes this is the line contating the name information
+  for item in myset:
+    searchstr = 'Name'
+    if searchstr in item:
+      targetlist = item.split()
+      myinstancename = targetlist[2]
+  return myinstancename
 
 ##########################
 #### END FUNCTON DEFS ####
