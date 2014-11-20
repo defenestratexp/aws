b09760978d34f720dd1711926c91f86219ce0813
Troy C. Thompson <troy.thompson@cognitivenetworks.com>
2014-11-19 17:09:06 -0800

    added getall function. Need to remove newlines and tabs

--- awscom.py
+++ awscom.py
@@ -17,6 +17,22 @@ import re
 #### FUNCTION DEFINITIONS ####
 ##############################
 
+# Function to obtain all instance information
+def getall(private_ip):
+  
+  #Command setup
+  LOCAL_AWS = 'aws'
+  LOCAL_AWS_SERVICE = 'ec2'
+  LOCAL_AWS_ARG1 = 'describe-instances'
+  DRYRUN = '--dry-run'
+  LOCAL_AWS_ARG2 = '--filters'
+  LOCAL_AWS_ARG3 = 'Name=private-ip-address,Values=%s' % private_ip
+ 
+  #Command execution
+  procout = subprocess.Popen([LOCAL_AWS, LOCAL_AWS_SERVICE, LOCAL_AWS_ARG1, LOCAL_AWS_ARG2, LOCAL_AWS_ARG3],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+  myset=list(procout.stdout)
+  print myset
+
 # Function to obtain an instance-id
 def getid(private_ip):
 
